3f7a760dd21712ade177d4f47522130f863748d6
Dave <8709533+DaveBathnes@users.noreply.github.com>
2024-04-15 21:30:40 +0100

    Update helpers.js

--- js/helpers.js
+++ js/helpers.js
@@ -2,5 +2,5 @@ var librariesAtRisk = {
   hexJson:
     'https://raw.githubusercontent.com/LibrariesHacked/uk-libraries-hexmap/main/services.hexjson',
   services: 'https://api.librarydata.uk/services/airtable?fields=Code,Name',
-  atRiskRegister: 'https://datawrapper.dwcdn.net/pnwZt/28/dataset.csv'
+  atRiskRegister: 'https://datawrapper.dwcdn.net/pnwZt/39/dataset.csv'
 }
